a5a8c53db59a59a03e5f53dd51e178a9579f3e5d
cub31993 <cub31993@hotmail.com>
2013-10-12 19:45:16 +0200

    Created project.

---
+++ src/se/kth/openglgame/Main.java
@@ -0,0 +1,11 @@
+package se.kth.openglgame;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: marek
+ * Date: 2013-10-12
+ * Time: 19:35
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+}
